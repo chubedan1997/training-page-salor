fa74e59bc8215062fb3ef8c942da9c18ab8b488a
chubedan1997 <nguyenhieu041197@gmail.com>
2022-05-01 00:08:10 +0700

    update page

--- js.js
+++ js.js
@@ -1,22 +1,30 @@
-// const menubar = document.querySelector('nav-menu')
-// console.log('hello')
-// const getStart = document.querySelector('col-btn')
-// menubar.addEventListener('click', bienDoi)
-// function bienDoi(){
-//     getStart=document.getElementById('col-btn').style.display='none'
-// }
 
 
 // attack app
 
 const itemAttack = document.querySelectorAll(".prortfolo_control");
 const itemData = document.querySelectorAll(".protfoloi-item");
+const cardItems = document.querySelectorAll(".CARD");
+const webItems = document.querySelectorAll(".WEB");
+const appItems = document.querySelectorAll(".APP");
 
 let index = 0;
+
 
 [...itemAttack].forEach(item => item.addEventListener("click", function(event){
     [...itemAttack].forEach(e => e.classList.remove("active"));
     event.target.classList.add("active");
+    
+    const dataKeyword = item.getAttribute("data-keyword")
+    itemData.forEach(data => {
+        if(data.getAttribute("data-keyword") === dataKeyword || dataKeyword === "ALL") {
+            data.classList.remove("hide-img")
+        } else {
+            data.classList.add("hide-img")
+        }
+
+    })
+    console.log(item);
 }));
 
 
